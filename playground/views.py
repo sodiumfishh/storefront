dff1434a1a7ad6d434f8de9dafd12bd29902bc24
Sami Nafis <78338492+sodiumfishh@users.noreply.github.com>
2023-09-15 12:36:03 +0600

    initial add

--- playground/views.py
+++ playground/views.py
@@ -1,13 +1,52 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
-
-def calculate():
-    x = 1
-    y = 2
-    return x
+from django.db.models import Q, F
+from django.db.models.aggregates import Count, Sum
+from store.models import Order, OrderItem, Product, Customer, Collection
 
 
 def say_hello(request):
-    x = calculate()
-    return render(request, 'hello.html', {'name': 'Mosh'})
+    ###### Q objects ######
+    # queryset = Product.objects.filter(Q(inventory__lt=10) | Q(unit_price__lt=20))
+
+    ###### F objects #####
+    # queryset = Product.objects.filter(inventory=F("unit_price"))
+
+    # queryset = (
+    #     Product.objects.filter(id__in=OrderItem.objects.values("product_id").distinct())
+    #     .order_by("title")
+    #     .values("title")
+    # )
+
+    ## Select_related
+    # queryset = Product.objects.select_related("collection").all()
+
+    """
+    prefetch_related
+    """
+    # queryset = (
+    #     Product.objects.prefetch_related("promotions")
+    #     .select_related("collection")
+    #     .all()
+    # )
+
+    # queryset = (
+    #     Order.objects.select_related("customer")
+    #     .prefetch_related("orderitem_set__product")
+    #     .order_by("-placed_at")[:5]
+    # )
+
+    # # result = Order.objects.filter(customer__id=1).aggregate(Count("id"))
+
+    # query = Order.objects.annotate(total_price=Sum("orderitem__unit_price"))
+
+    # list_query = list(query)
+
+    # queryset = Product.objects.prefetch_related("collection").all()
+
+    # context = {
+    #     "name": "Mosh",
+    #     # "customers": list(customer_queryset),
+    #     "products": list(queryset),
+    # }
+
+    return render(request, "hello.html", context)
--- store/models.py
+++ store/models.py
@@ -9,7 +9,8 @@ class Promotion(models.Model):
 class Collection(models.Model):
     title = models.CharField(max_length=255)
     featured_product = models.ForeignKey(
-        'Product', on_delete=models.SET_NULL, null=True, related_name='+')
+        "Product", on_delete=models.SET_NULL, null=True, related_name="+"
+    )
 
 
 class Product(models.Model):
@@ -24,14 +25,14 @@ class Product(models.Model):
 
 
 class Customer(models.Model):
-    MEMBERSHIP_BRONZE = 'B'
-    MEMBERSHIP_SILVER = 'S'
-    MEMBERSHIP_GOLD = 'G'
+    MEMBERSHIP_BRONZE = "B"
+    MEMBERSHIP_SILVER = "S"
+    MEMBERSHIP_GOLD = "G"
 
     MEMBERSHIP_CHOICES = [
-        (MEMBERSHIP_BRONZE, 'Bronze'),
-        (MEMBERSHIP_SILVER, 'Silver'),
-        (MEMBERSHIP_GOLD, 'Gold'),
+        (MEMBERSHIP_BRONZE, "Bronze"),
+        (MEMBERSHIP_SILVER, "Silver"),
+        (MEMBERSHIP_GOLD, "Gold"),
     ]
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
@@ -39,22 +40,24 @@ class Customer(models.Model):
     phone = models.CharField(max_length=255)
     birth_date = models.DateField(null=True)
     membership = models.CharField(
-        max_length=1, choices=MEMBERSHIP_CHOICES, default=MEMBERSHIP_BRONZE)
+        max_length=1, choices=MEMBERSHIP_CHOICES, default=MEMBERSHIP_BRONZE
+    )
 
 
 class Order(models.Model):
-    PAYMENT_STATUS_PENDING = 'P'
-    PAYMENT_STATUS_COMPLETE = 'C'
-    PAYMENT_STATUS_FAILED = 'F'
+    PAYMENT_STATUS_PENDING = "P"
+    PAYMENT_STATUS_COMPLETE = "C"
+    PAYMENT_STATUS_FAILED = "F"
     PAYMENT_STATUS_CHOICES = [
-        (PAYMENT_STATUS_PENDING, 'Pending'),
-        (PAYMENT_STATUS_COMPLETE, 'Complete'),
-        (PAYMENT_STATUS_FAILED, 'Failed')
+        (PAYMENT_STATUS_PENDING, "Pending"),
+        (PAYMENT_STATUS_COMPLETE, "Complete"),
+        (PAYMENT_STATUS_FAILED, "Failed"),
     ]
 
     placed_at = models.DateTimeField(auto_now_add=True)
     payment_status = models.CharField(
-        max_length=1, choices=PAYMENT_STATUS_CHOICES, default=PAYMENT_STATUS_PENDING)
+        max_length=1, choices=PAYMENT_STATUS_CHOICES, default=PAYMENT_STATUS_PENDING
+    )
     customer = models.ForeignKey(Customer, on_delete=models.PROTECT)
 
 
@@ -68,8 +71,7 @@ class OrderItem(models.Model):
 class Address(models.Model):
     street = models.CharField(max_length=255)
     city = models.CharField(max_length=255)
-    customer = models.ForeignKey(
-        Customer, on_delete=models.CASCADE)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
 
 
 class Cart(models.Model):
